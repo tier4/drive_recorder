75d6758eed8f06152f59f1926610607604e3ffbb
nobuotakamasa <nobuo.takamasa@tier4.jp>
2019-11-22 10:29:04 +0900

    中間ファイルの削除

--- build/pubsub/pubsubtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/takamasa/catkin_ws/src/pubsub/pubsubtest
-# Build directory: /home/takamasa/catkin_ws/build/pubsub/pubsubtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

2d07bc131fe6a4472608d4084743999353cdfd1c
nobuotakamasa <nobuo.takamasa@tier4.jp>
2019-11-26 14:40:51 +0900

    decision_makerからのpublishを送るように変更

--- src/drive_recorder/src/drive_recorder_test.cpp
+++ src/drive_recorder/src/drive_recorder_test.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2019 Autoware Foundation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Header.h>
@@ -11,21 +29,24 @@ using boost::interprocess::open_only;
 using boost::interprocess::create_only;
 static ros::Time start_time;
 
-//static constexpr const char* SHM_NAME = "SharedMemoryForVitalMonitor";
-//static constexpr int SHM_SIZE = 65536;
-//static constexpr unsigned int SHM_TH_COUNTER = 3;
-//static constexpr unsigned int SHM_COUNTER_MAX = 10000;
-
-
 int main(int argc, char**argv){
     ros::init(argc, argv, "emergency_handler");
     ros::NodeHandle nh;
     ros::Publisher recordcmd_pub_ = nh.advertise<std_msgs::Header>("record_cmd", 1, true);
+    ros::Publisher decition_pub_ = nh.advertise<std_msgs::String>("decision_maker/state", 1, true);
     start_time = ros::Time::now();
     shared_memory_object::remove(SHM_NAME);
     managed_shared_memory shm(create_only, SHM_NAME, SHM_SIZE);
     bool* stopReq = shm.construct<bool>("SHM_DRStopRequest")();
-    ros::Rate rate(0.005);
+    ros::Rate rate0(0.01);
+    ros::Rate rate1(0.01);
+    ros::Rate rate2(0.1);
+    ros::Rate rate3(1);
+    std_msgs::String evergency;
+    evergency.data = "VehicleEmergency";
+    std_msgs::String ready;
+    ready.data = "VehicleReady";
+
     while (ros::ok())
     {
         ros::spinOnce();
@@ -33,11 +54,16 @@ int main(int argc, char**argv){
         header_msg.stamp = start_time;
         ROS_INFO("publish");
         recordcmd_pub_.publish(header_msg);
-        rate.sleep();
+        rate0.sleep();
         ROS_INFO("stop request start");
-        *stopReq = true;
-        rate.sleep();
-        *stopReq = false;
+        *stopReq = true;//共有メモリのフラグを立てる
+        rate1.sleep();
+        *stopReq = false;//共有メモリのフラグを落とす
         ROS_INFO("stop request end");
+        rate2.sleep();
+        ROS_INFO("publish2");
+        decition_pub_.publish(evergency);
+        rate2.sleep();
+        decition_pub_.publish(ready);
     }
 }

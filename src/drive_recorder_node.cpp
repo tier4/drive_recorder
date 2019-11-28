4121cf278a91f834ba71173e695b443b94dab85f
nobuotakamasa <nobuo.takamasa@tier4.jp>
2019-11-28 12:22:23 +0900

    fix for ROS c++ syte guide

--- src/drive_recorder_node.cpp
+++ src/drive_recorder_node.cpp
@@ -37,11 +37,12 @@ using std::to_string;
 class DriveRecorder 
 {
   //タイマが満了してから、ログのファイルを保存するために遡る時間
-  ros::Duration record_time_period;
+  ros::Duration record_time_period_;
   //異常発生時に生成するタイマの時間
-  ros::Duration timer_exprie_period;
+  ros::Duration timer_exprie_period_;
   //rosbagがlogを出力する場所
-  string src_dirname = "log/";
+  string src_dirname_;
+  string dst_dirname_;
 
   static void showTopics()
   {
@@ -56,29 +57,28 @@ class DriveRecorder
   }
 
   //異常発生からのtimer待ちのcallback
-  void timer_callback(const ros::TimerEvent& te)
+  void timerCallback(const ros::TimerEvent& te)
   {
-    emflag  = emergency_progress;
+    emflag_  = emergency_progress;
     //現在時刻
     ros::Time timer_end   = ros::Time::now();
     //どの時点からのファイルをコピーしなくはならないか
-    ros::Time timer_begin = timer_end - record_time_period;
+    ros::Time timer_begin = timer_end - record_time_period_;
     ROS_INFO("drive recorder timer expired (%d:%d)-(%d:%d)", 
       timer_begin.sec, timer_begin.nsec,
       timer_end.sec, timer_end.nsec);
 
     //コピー先の場所の名前。その時のROS時刻(秒)をディレクトリ名とする。
-    string dst_dirname = to_string(timer_begin.sec) + "/";
+    string dst_dirname = dst_dirname_ + to_string(timer_begin.sec) + "/";
     fs::path dst_directory(dst_dirname);
     //struct stat stat_buf;
-    fs::path src_directory(src_dirname);
+    fs::path src_directory(src_dirname_);
     fs::directory_iterator end;
     fs::create_directory(dst_directory);
     //logディレクトリのフィアルを列挙
     for( fs::directory_iterator src_file(src_directory); src_file != end; src_file++)
     {
       try{
-        //.. .だったら、飛ばす。
         auto ext = src_file->path().extension();
         if( ext != ".bag" && ext != ".active" )
           continue;
@@ -100,13 +100,13 @@ class DriveRecorder
       }
     }
     ROS_INFO("emergency_done");
-    emflag = emergency_progress_done;
+    emflag_ = emergency_progress_done;
   }
 
-  ros::NodeHandle n;
+  ros::NodeHandle n_;
 
   //emergency発生から開始するoneshotタイマ
-  ros::Timer timer;
+  ros::Timer timer_;
 
   //現在の処理中を示す
   enum emergency_state
@@ -116,35 +116,35 @@ class DriveRecorder
     emergency_progress = 2,//コピー中
     emergency_progress_done = 3,//コピー完了。emergecy解除待ち
   };
-  emergency_state emflag = emergency_none;
-  bool decision_maker_state_emergency = false;
-  void decision_maker_state_callback(const std_msgs::String& msg)
+  emergency_state emflag_ = emergency_none;
+  bool decision_maker_state_emergency_ = false;
+  void decisionMakerStateCallback(const std_msgs::String& msg)
   {
     ROS_INFO("/decision_maker/state subscribed (%s) ", msg.data.c_str());
     if( msg.data == "VehicleEmergency" )
     {
-      decision_maker_state_emergency = true;
+      decision_maker_state_emergency_ = true;
     }
     else
     {
-      decision_maker_state_emergency = false;
+      decision_maker_state_emergency_ = false;
     }
   }
   //emergency_handlerのrecord_cmdを受けるcallback
-  void record_cmd_callback(const std_msgs::Header header_msg)
+  void recordCmdCallback(const std_msgs::Header header_msg)
   {
     ROS_INFO("drive recorder subscribed (%d:%d) ", header_msg.stamp.sec, header_msg.stamp.nsec);
-    //ROS_ASSERT(emflag == none);
-    start_timer();
+    //ROS_ASSERT(emflag_ == none);
+    startTimer();
   }
   //タイマ開始。
-  void start_timer()
+  void startTimer()
   {
-    if( emflag == emergency_none )
+    if( emflag_ == emergency_none )
     {
-      emflag = emergency_requested;
-      ROS_INFO("start timer (%d) ", timer_exprie_period.sec );
-      timer = n.createTimer(timer_exprie_period, &DriveRecorder::timer_callback, this, true);//一定時間後にtimer_callbackを呼ぶ。oneshot = trueなので一回で終了する。
+      emflag_ = emergency_requested;
+      ROS_INFO("start timer (%d) ", timer_exprie_period_.sec );
+      timer_ = n_.createTimer(timer_exprie_period_, &DriveRecorder::timerCallback, this, true);//一定時間後にtimerCallbackを呼ぶ。oneshot = trueなので一回で終了する。
     }
     else
     {
@@ -156,7 +156,7 @@ class DriveRecorder
   void stopRequested()
   {
     ROS_INFO("stop requedted");
-    start_timer();
+    startTimer();
   }
   //unit未満の端数を繰り上げる。桁溢れ、０割に注意
   static int roundup(int num, int unit)
@@ -173,19 +173,28 @@ class DriveRecorder
     ROS_ASSERT(result > 0);
     return result;
   }
+  ros::NodeHandle private_nh_;
+
+  //bag_period rosbagが生成するlogファイルの時間間隔(秒)
+  const int default_before = 10 * 60;
+  //after異常発生後にどれだけ待ってからコピーを開始するか(秒)
+  const int default_after = 10;
+  //before異常発生時からどれだけの時間遡ってコピーするか(秒)
+  const int default_bag_period = 60;
   public:
   void run()
   {
-    ros::Subscriber sub = n.subscribe("record_cmd", 50, &DriveRecorder::record_cmd_callback, this);
-    ros::Subscriber sub2 = n.subscribe("decision_maker/state", 50, &DriveRecorder::decision_maker_state_callback, this);
+    ros::Subscriber sub = n_.subscribe("record_cmd", 50, &DriveRecorder::recordCmdCallback, this);
+    ros::Subscriber sub2 = n_.subscribe("decision_maker/state", 50, &DriveRecorder::decisionMakerStateCallback, this);
     ros::Rate rate(1);
     managed_shared_memory shm(open_only, SHM_NAME);
+
     //共有メモリのフラグ
     volatile bool* stopReq = shm.find<bool>("SHM_DRStopRequest").first;
     while (ros::ok())
     {
       ros::spinOnce();
-      switch(emflag)
+      switch(emflag_)
       {
         case emergency_none:
           if(*stopReq == true)
@@ -196,10 +205,10 @@ class DriveRecorder
         case emergency_progress_done:
           //ファイルのコピーが終わった状態。
           //共有メモリのフラグが落ちるのを待つ。
-          if(*stopReq == false && decision_maker_state_emergency == false)
+          if(*stopReq == false && decision_maker_state_emergency_ == false)
           {
             ROS_INFO("emergency_progress_done -> none");
-            emflag = emergency_none;
+            emflag_ = emergency_none;
           }
           //showTopics();
           break;       
@@ -209,26 +218,34 @@ class DriveRecorder
       rate.sleep();
     }
   }
-  //before異常発生時からどれだけの時間遡ってコピーするか(秒)
-  //after異常発生後にどれだけ待ってからコピーを開始するか(秒)
-  //bag_period rosbagが生成するlogファイルの時間間隔(秒)
-  DriveRecorder(time_t _before, time_t _after, time_t bag_period)//: private_nh_("~")
+  DriveRecorder() : private_nh_("~")
   {
+    int _before;
+    int _after;
+    int _bag_period;
+    // parameter settings
+    private_nh_.param<int>("before_time", _before, default_before);
+    private_nh_.param<int>("after_time", _after,  default_after);
+    private_nh_.param<int>("bag_period", _bag_period, default_bag_period);
+    private_nh_.param<string>("log_dir", src_dirname_, "log");
+    private_nh_.param<string>("log_out", dst_dirname_, "backup");
+    dst_dirname_ += "/";
+    ROS_INFO("%d %d %d %s %s", _before, _after, _bag_period, src_dirname_.c_str(), dst_dirname_.c_str());
     //bag_period分切り上げる。
-    _after = roundup(_after, bag_period);
-    _before = roundup(_before, bag_period);
+    _after = roundup(_after, _bag_period);
+    _before = roundup(_before, _bag_period);
     ros::Duration before(_before);
     ros::Duration after(_after);
 
-    record_time_period = before + after;
-    timer_exprie_period = after;
+    record_time_period_ = before + after;
+    timer_exprie_period_ = after;
   }
 };
 
 int main(int argc, char**argv)
 {
   ros::init(argc, argv, "drive_recoorder");
-  DriveRecorder d(10 * 60, 10, 60);
+  DriveRecorder d;
   d.run();
   return 0;
 }

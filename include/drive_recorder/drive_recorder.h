58c9786558964f672160ef0fcc1d2f39b86f1966
nobuotakamasa <nobuo.takamasa@tier4.jp>
2019-12-06 10:16:43 +0900

    ROS_INFO->ROS_DEBUG

--- include/drive_recorder/drive_recorder.h
+++ include/drive_recorder/drive_recorder.h
@@ -134,7 +134,7 @@ void DriveRecorder::timerCallback(const ros::TimerEvent& te)
   ros::WallTime timer_end   = ros::WallTime::now();
   //calcurate when we have to go back.
   ros::WallTime timer_begin = timer_end - record_time_period_;
-  ROS_INFO("drive recorder timer expired (%d:%d)-(%d:%d)", 
+  ROS_DEBUG("drive recorder timer expired (%d:%d)-(%d:%d)", 
     timer_begin.sec, timer_begin.nsec,
     timer_end.sec, timer_end.nsec);
   //directory name for dstinatin. ROS time become the name.
@@ -158,7 +158,7 @@ void DriveRecorder::timerCallback(const ros::TimerEvent& te)
       {
         auto dst_filename = dst_dirname + src_file->path().filename().string();
         fs::path dst(dst_filename);
-        ROS_INFO("log file:%s -> %s mod time:%d(%d)", src_file->path().string().c_str(), dst_filename.c_str(), (int)mod_t, (int)diff );
+        ROS_DEBUG("log file:%s -> %s mod time:%d(%d)", src_file->path().string().c_str(), dst_filename.c_str(), (int)mod_t, (int)diff );
         if( ext == ".bag" )
         {
             fs::rename(src_file->path(), dst);
@@ -171,20 +171,19 @@ void DriveRecorder::timerCallback(const ros::TimerEvent& te)
     }
     catch(boost::filesystem::filesystem_error& e) //rosbag execute asyncronouse. so rosbag may rename .active to .bag. 
     {
-        ROS_INFO("copy_file failed");
+        ROS_DEBUG("copy_file failed");
     }
   }
-  ROS_INFO("emergency_done");
-  emflag_ = emergency_progress_done;
+  ROS_DEBUG("emergency_done");
 }
 void DriveRecorder::decisionMakerStateCallback(const std_msgs::String& msg)
 {
   decision_maker_state_emergency_ = ( msg.data.find("VehicleEmergency") == string::npos ) ? false : true;
-  ROS_INFO("/decision_maker/state subscribed (%s, %d) ", msg.data.c_str(), decision_maker_state_emergency_);
+  ROS_DEBUG("/decision_maker/state subscribed (%s, %d) ", msg.data.c_str(), decision_maker_state_emergency_);
 }
 void DriveRecorder::recordCmdCallback(const std_msgs::Header header_msg)
 {
-  ROS_INFO("drive recorder subscribed (%d:%d) ", header_msg.stamp.sec, header_msg.stamp.nsec);
+  ROS_DEBUG("drive recorder subscribed (%d:%d) ", header_msg.stamp.sec, header_msg.stamp.nsec);
   startTimer();
 }
 
@@ -192,19 +191,18 @@ void DriveRecorder::startTimer()
 {
   if( emflag_ == emergency_none )
   {
-    emflag_ = emergency_requested;
-    ROS_INFO("start timer (%d) ", timer_expire_period_.sec );
+    ROS_DEBUG("start timer (%d) ", timer_expire_period_.sec );
     timer_ = n_.createTimer(timer_expire_period_, &DriveRecorder::timerCallback, this, true);//oneshot = true, so timerCallbak will be called onece.
   }
   else
   {
-    ROS_INFO("timer already started!!!");
+    ROS_DEBUG("timer already started!!!");
   }
 }
 
 void DriveRecorder::stopRequested()
 {
-  ROS_INFO("stop requedted");
+  ROS_DEBUG("stop requedted");
   startTimer();
 }
 
@@ -223,8 +221,7 @@ void DriveRecorder::timerPollingCallback(const ros::TimerEvent& te)
       //waiting for shared memory flag is false and /decision_make_state is not VehicleEmergency.
       if(stop_requested_.is_request_received() == false && decision_maker_state_emergency_ == false)
       {
-        ROS_INFO("emergency_progress_done -> none");
-        emflag_ = emergency_none;
+        ROS_DEBUG("emergency_progress_done -> none");
       }
       break;       
     default:
@@ -251,7 +248,7 @@ DriveRecorder::DriveRecorder() : private_nh_("~")
   fs::path src_directory(src_dirname_);
   fs::create_directories(src_directory);
 
-  ROS_INFO("%d %d %d %s %s", _before, _after, _bag_period, src_dirname_.c_str(), dst_dirname_.c_str());
+  ROS_DEBUG("%d %d %d %s %s", _before, _after, _bag_period, src_dirname_.c_str(), dst_dirname_.c_str());
   //round up by bag_period
   _before = roundup(_before, _bag_period);
 
